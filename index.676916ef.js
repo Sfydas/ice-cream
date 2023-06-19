b46973bf1e21cb0ab32cc6b430fafaf8d6f3027f
Sfydas <74378614+Sfydas@users.noreply.github.com>
2023-06-19 12:51:35 +0000

    Deploying to gh-pages from @ Sfydas/ice-cream@43a6e2a2715ca4da3e2f1e8ad0673abfaf28e62e 🚀

---
+++ index.676916ef.js
@@ -0,0 +1,2 @@
+SwiperCore.use([Navigation,Pagination]),new Swiper(".swiper-container",{spaceBetween:30,effect:"fade",pagination:{el:".swiper-pagination",clickable:!0},navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"}});
+//# sourceMappingURL=index.676916ef.js.map
